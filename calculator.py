66a6944f6fa83b4267c3acb233d615b370a63554
padolha <matheus.padilha998@gmail.com>
2025-09-09 11:05:45 -0300

    Commit 3 - Divisão e Multiplicação
    Adição de divisão e multiplicação dentro da calc

--- calculator.py
+++ calculator.py
@@ -10,3 +10,11 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error! Division by zero."
+    return a / b
