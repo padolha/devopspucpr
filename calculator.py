60387f002c46ffd7c21e5f9acf2c62c91f735842
padolha <matheus.padilha998@gmail.com>
2025-09-09 11:01:39 -0300

    Comit 1 - Calc básica
    Criação da calc com adição e subtração.

---
+++ calculator.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def main():
+    print("Calculadora Simples")
+    # TODO: Implementar a lógica de interação com o usuário
+
+if __name__ == "__main__":
+    main()
